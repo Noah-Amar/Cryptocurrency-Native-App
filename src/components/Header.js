b05f4cca54775b932c2d82d7cb316394dba605c3
Noah Amar <ne.amar@outlook.com>
2019-03-28 01:04:13 -0500

    styling, redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class Header extends React.Component{
     render(){
         return(
-            <View>
-                <Text>Cryptocurrency App</Text>
+            <View style={headerContainer}>
+                <Text style={header}>Cryptocurrency App</Text>
             </View>
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    headerContainer:{
+        display: "flex",
+        marginTop: 55,
+        alignItems: "center",
+    },
+    header:{
+        fontWeight: "bold",
+        fontSize: 20,
+    }
+})
+
+const { headerContainer, header } = styles;
