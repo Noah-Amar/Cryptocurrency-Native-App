f1cf14b7f5dc92455cdaba47d36a07d8a0d31936
Noah Amar <ne.amar@outlook.com>
2019-03-28 01:19:40 -0500

    add store to app.js

--- App.js
+++ App.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
 import { Header } from './src/components';
+import Store from './src/Store';
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store={Store}>
       <View>
         <Header />
       </View>
+      </Provider>
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
